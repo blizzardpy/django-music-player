9f9007d652fa4014e578eb36c889c20eea4150d0
Amirkian Kiani <74903450+DarkSnowJR@users.noreply.github.com>
2021-10-12 23:20:19 +0330

    player

--- MusicPlayer/urls.py
+++ MusicPlayer/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.contrib.staticfiles.urls import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include("App.urls")),
+
 ]
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
